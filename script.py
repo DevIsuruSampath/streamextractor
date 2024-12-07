c5387ea26abdf295d1e062b586b426d902d85a4d
DevIsuruSampath <devisurusampath@gmail.com>
2024-12-07 21:14:35 +0530

    first commit

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @hellohoneybunnyy
+
+
+class Script(object):
+
+    START_MSG = """<b>Hello {},
+
+I can extract audio and subtitles from video files.
+
+Send me any video file and extract what you want :).
+
+See <i>help</i> for more details.</b>
+
+@I_Am_Devil_Mafia"""
+
+
+    HELP_MSG = """<b>Hi, Follow these Steps..</b>
+
+🌀 <i>Send me any Valid video file.</i>
+
+🌀 <i>Click Download and Process button to download the file to my server</i>
+
+🌀 <i>Wait till I process the video!</i>
+
+🌀 <i>Now select the stream you need to extract!</i>
+
+@I_Am_Devil_Mafia"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : IU Streams Extractor Bot</b>
+
+⭕️<b>Creater :</b> @I_Am_Devil_Mafia  
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.4.0</a> 
+
+"""
